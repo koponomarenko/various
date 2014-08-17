e0a62cf156c5bc81c587a3446ffaaf53647566ba
Konstantin Ponomarenko <konstantin.ponomarenko@gmail.com>
2014-08-17 16:22:49 +0300

    added two last assignments

--- coursera/programming_for_everybody_python/week_09.py
+++ coursera/programming_for_everybody_python/week_09.py
@@ -1,6 +1,24 @@
-# Write a program to read through the mbox-short.txt and figure out who has the sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+# Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
 
 
-name = raw_input("Enter file:")
-if len(name) < 1 : name = "mbox-short.txt"
-handle = open(name)
+name = raw_input("Enter file: ")
+if len(name) < 1:
+    name = "data/mbox-short.txt"
+fh = open(name)
+stat = dict()
+for line in fh:
+    if line.startswith('From') and not line.startswith('From:'):
+        words = line.split()
+        stat[words[1]] = stat.get(words[1], 0) + 1
+
+max_emails = None
+sender = None
+for key, value in stat.items():
+    if max_emails is None:
+        max_emails = value
+        sender = key
+    elif max_emails < value:
+        max_emails = value
+        sender = key
+
+print sender, max_emails
--- coursera/programming_for_everybody_python/week_10.py
+++ coursera/programming_for_everybody_python/week_10.py
@@ -2,6 +2,23 @@
 #   From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 # Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below. Note that the autograder does not have support for the sorted() function.
 
-name = raw_input("Enter file:")
-if len(name) < 1 : name = "mbox-short.txt"
-handle = open(name)
+name = raw_input("Enter file: ")
+if len(name) < 1:
+    name = "data/mbox-short.txt"
+fh = open(name)
+
+stat = dict()
+for line in fh:
+    if line.startswith('From') and not line.startswith('From:'):
+        words = line.split()
+        time = words[5].split(':')
+        stat[time[0]] = stat.get(time[0], 0) + 1
+
+# create a list of tuples from the dictionary
+lst =  stat.items()
+
+# sort by key
+lst.sort()
+
+for key, val in lst:
+    print key, val
